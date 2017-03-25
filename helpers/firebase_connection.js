745cf2a70e5c667d808d53bce8c7aabe4f56854f
equitysplit <equity.split.m@gmail.com>
2017-03-24 17:25:40 -0700

    improved firebase credentials, added env variables instead of hardcoded config file

--- helpers/firebase_connection.js
+++ helpers/firebase_connection.js
@@ -8,11 +8,16 @@ var beautify = require("json-beautify");
 var firebase_credentials = require("../config/firebase_credentials.json");
 
 var config = {
-    apiKey:             firebase_credentials.apiKey,
-    authDomain:         firebase_credentials.authDomain,
-    databaseURL:        firebase_credentials.databaseURL,
-    storageBucket:      firebase_credentials.storageBucket,
-    messagingSenderId:  firebase_credentials.messagingSenderId
+    // apiKey:             firebase_credentials.apiKey,
+    // authDomain:         firebase_credentials.authDomain,
+    // databaseURL:        firebase_credentials.databaseURL,
+    // storageBucket:      firebase_credentials.storageBucket,
+    // messagingSenderId:  firebase_credentials.messagingSenderId
+    apiKey:             process.env.FIREBASE_API_KEY,
+    authDomain:         process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL:        process.env.FIREBASE_DATABASE_URL,
+    storageBucket:      process.env.FIREBASE_MESSAGING_SENDER_ID,
+    messagingSenderId:  process.env.FIREBASE_STORAGE_BUCKET
 };
 firebase.initializeApp(config);
 

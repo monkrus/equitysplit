b43aa21b2c550ab00f5d091aac9469aa9e00056b
equitysplit <equity.split.m@gmail.com>
2017-03-26 21:31:38 -0700

    cleared input text on submit in forms

--- public/js/home.js
+++ public/js/home.js
@@ -1,5 +1,16 @@
 /**
  * Created by equitysplit on 3/26/17.
  */
-$('#message_button').notify('Message sent!');
-$('#subscribe_button').notify('Subscription successful!');
+$(document).ready(function () {
+    $('#message_button').click(function(){
+        $('#message_button').notify('Message sent!', 'success');
+    });
+    $('#subscribe_button').click(function(){
+        $('#subscribe_button').notify('Subscription successful!', 'success');
+    });
+
+    $(".submit").click(function() {
+        $(this).closest('form').find("input[type=text], textarea").val("");
+        $(this).closest('form').find("input[type=email], textarea").val("");
+    });
+});
